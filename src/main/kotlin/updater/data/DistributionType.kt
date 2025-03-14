108c6dcc673abadef54df84c57899fc47c3a6bda
zmilla93 <zmilla93@gmail.com>
2025-03-14 02:50:53 -0500

    Update DistributionType.kt

---
+++ src/main/kotlin/updater/data/DistributionType.kt
@@ -0,0 +1,29 @@
+package io.github.zmilla93.updater.data
+
+import java.nio.file.Paths
+import kotlin.io.path.exists
+
+enum class DistributionType {
+
+    JAR,
+    WINDOWS_MSI,
+    WINDOWS_PORTABLE,
+    // TODO : Mac & Linux
+    ;
+
+    companion object {
+
+        var hasRunCheck = false
+        private var current: DistributionType = JAR
+
+        fun checkCurrent(launcherPath: String): DistributionType {
+            if (hasRunCheck) return current
+            val launcher = Paths.get(launcherPath)
+            val appFolder = launcher.parent
+            if (appFolder.resolve(".package").exists()) current = WINDOWS_MSI
+            else if (appFolder.resolve(".jpackage.xml").exists()) current = WINDOWS_PORTABLE
+            return current
+        }
+    }
+
+}
